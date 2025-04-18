d81a1123ab74ffe7f479021548bf6353e807bc6f
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-04-18 18:13:43 +0530

    layout.js

---
+++ layout.js
@@ -0,0 +1,18 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black">
+        {children}
+      </body>
+    </html>
+  );
+}
